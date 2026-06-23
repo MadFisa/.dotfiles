7dd6949345b69db882a6e34ad9a79c4aa6f80776
Asif Mandayapuram <asifmp97@gmail.com>
2026-06-23 18:02:55 +0530

    feat(sswidl): Added niri to handle things properly

--- fish/.config/fish/functions/sswidlde.fish
+++ fish/.config/fish/functions/sswidlde.fish
@@ -14,5 +14,6 @@ function sswidlde
 
     # Run IDL inside an isolated Wine Virtual Desktop.
     # Note: 1260x680 fits perfectly inside a 1280x720 Weston window without hiding Weston's top bar.
-    wine explorer /desktop=SolarSoft,1260x680 "/mnt/efd42152-14ab-413d-9d2a-6ebed080bc74/idl_wine/drive_c/Program Files/Exelis/IDL85/bin/bin.x86_64/idlde.exe" $argv
+    #wine explorer /desktop=SolarSoft,1260x680 "/mnt/efd42152-14ab-413d-9d2a-6ebed080bc74/idl_wine/drive_c/Program Files/Exelis/IDL85/bin/bin.x86_64/idlde.exe" $argv
+    wine explorer /desktop=SolarSoft,1920x1080 "/mnt/efd42152-14ab-413d-9d2a-6ebed080bc74/idl_wine/drive_c/Program Files/Exelis/IDL85/bin/bin.x86_64/idlde.exe" $argv
 end
