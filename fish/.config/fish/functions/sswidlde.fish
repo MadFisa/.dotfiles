4e9702a3c76ff45b0ad85cbbc9dd4602eddfacc1
Asif Mandayapuram <asifmp97@gmail.com>
2026-06-23 14:16:15 +0530

    feat(sswidl):Ability to run inside its own virtual desktop

--- fish/.config/fish/functions/sswidlde.fish
+++ fish/.config/fish/functions/sswidlde.fish
@@ -9,9 +9,10 @@ function sswidlde
     set -x SSW_INSTR "forward chianti xrt eit trace secchi aia pfss swap lasco"
     set -x HOST "lmsal.com"
 
-    # Suppress Wine debug messages (optional, makes it cleaner)
+    # Suppress Wine debug messages
     set -x WINEDEBUG -all
 
-    # Run IDL Console directly
-    wine "/mnt/efd42152-14ab-413d-9d2a-6ebed080bc74/idl_wine/drive_c/Program Files/Exelis/IDL85/bin/bin.x86_64/idlde.exe" $argv
+    # Run IDL inside an isolated Wine Virtual Desktop.
+    # Note: 1260x680 fits perfectly inside a 1280x720 Weston window without hiding Weston's top bar.
+    wine explorer /desktop=SolarSoft,1260x680 "/mnt/efd42152-14ab-413d-9d2a-6ebed080bc74/idl_wine/drive_c/Program Files/Exelis/IDL85/bin/bin.x86_64/idlde.exe" $argv
 end
